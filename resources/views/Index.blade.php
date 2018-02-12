22880d1912691074db3d50e5b294e684071421bd
monkeyteacher <peter1180041@gmail.com>
2018-02-12 15:08:40 +0800

    修改首頁資料顯示

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -15,21 +15,21 @@
                     <th colspan='{{ count($data['Stories'])+3 }}'>課程列表</th>
                 </tr>
                 <tr>
-                    <th rowspan='{{ count($data['Stories'])+3 }}'>開放</th>
+                    <th rowspan='{{ count($data['Stories'])+3 }}'>{{ $data['Course'][0]['IsOpen']?'開放':'未開放' }}</th>
                     <th>編號</th>
                     <th colspan='2'>課程名稱</th>
-                    <th colspan='2'>課程完成人數</th>
-                    <th>課程參與人數</th>
+                    <th colspan='2'>學生課程參與次數</th>
+                    <th>學生課程完成次數</th>
                     <th>完成率</th>
                     <th><a href= '#'>[新增課程(導入題庫)]</a>
                     </th>
                 </tr>
                 <tr>
-                    <td>1</td>
-                    <td colspan='2'>經濟未來學</td>
-                    <td colspan='2'>89</td>
-                    <td>99</td>
-                    <td>89%</td>
+                    <td>{{ $data['Course'][0]['CourseID'] }}</td>
+                    <td colspan='2'>{{ $data['Course'][0]['CourseName'] }}</td>
+                    <td colspan='2'>{{ $data['Course'][0]['completedNum'] }}</td>
+                    <td>{{ $data['Course'][0]['participateNum'] }}</td>
+                    <td>{{ (int)($data['Course'][0]['CompletionRate']*100).'%' }}</td>
                     <td class='text-right'><a href='#'>[編輯]</a><a href='#'>[列出章節列表]</a></td>
                 </tr>
                 <tr>
@@ -43,7 +43,7 @@
                         <td>{{ $data['Stories'][$i]['IsOpen']?'開放':'未開放' }}</td>
                         <td>{{ $data['Stories'][$i]['Priority'] }}</td>
                         <td colspan='2' class='StoryName'>{{ $data['Stories'][$i]['StoryName'] }}</td>
-                        <td colspan='4' class='text-right'><a class='stroy_detail' href="{{ route('MainHome',['StoreID'=>$data['Stories'][$i]['StoryID']]) }}">[查看詳細]</a></td>
+                        <td colspan='4' class='text-right'><a class='stroy_detail' href="{{ route('MainHome',['CourseID'=>$data['Course'][0]['CourseID'],'StoreID'=>$data['Stories'][$i]['StoryID']]) }}">[查看詳細]</a></td>
                     </tr>
                 @endfor
             </table>
@@ -71,9 +71,9 @@
                             <td>{{ $data['Knowledges'][$i]['IsOpen']?'開放':'未開放' }}</td>
                             <td>{{ $data['Knowledges'][$i]['KnowID'] }}</td>
                             <td>{{ $data['Knowledges'][$i]['KnowName'] }}</td>
-                            <td>0</td>
-                            <td>0</td>
-                            <td>0%</td>
+                            <td>{{ $data['Knowledges'][$i]['completedNum'] }}</td>
+                            <td>{{ $data['Knowledges'][$i]['participateNum'] }}</td>
+                            <td>{{ (int)($data['Knowledges'][$i]['CompletionRate']*100).'%' }}</td>
                             <td colspan='3' class='text-right'><a href='#'>[查看詳細分析]</a></td>
                         </tr>
                     @endfor
